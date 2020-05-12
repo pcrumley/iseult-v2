ef9b6c00676034b152719000fbb36bb9c3f475d6
Patrick Crumley <patrick.crumley@gmail.com>
2020-05-12 13:32:40 -0400

    pep8

--- src/playback_bar.py
+++ src/playback_bar.py
@@ -2,6 +2,7 @@ import tkinter as Tk
 from tkinter import ttk
 from main_settings_window import SettingsFrame
 
+
 class playbackBar(Tk.Frame):
 
     """
@@ -13,7 +14,7 @@ class playbackBar(Tk.Frame):
     def __init__(self, oengus, tstep_param):
         Tk.Frame.__init__(self)
         self.oengus = oengus
-        self._cur_sim = 0 # A way to hold the current simulation
+        self._cur_sim = 0  # A way to hold the current simulation
         self.play_pressed = False
         self.settings_window = None
         # This param should be the time-step of the simulation
@@ -42,7 +43,7 @@ class playbackBar(Tk.Frame):
             *tuple(
                 map(lambda x: self.oengus.sims[x].name,
                     self.oengus.sims_shown)))
-        self.sim_menu.pack(side=Tk.LEFT, fill=Tk.BOTH, expand = 0)
+        self.sim_menu.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
         # the Check boxes for the dimension
         # An entry box that will let us choose the time-step
         ttk.Label(self, text='n= ').pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
@@ -54,7 +55,7 @@ class playbackBar(Tk.Frame):
 
         # the entry box
         self.txt_enter = ttk.Entry(self, textvariable=self.tstep, width=6)
-        self.txt_enter.pack(side=Tk.LEFT, fill=Tk.BOTH, expand = 0)
+        self.txt_enter.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
 
         # A slider that will show the progress in the simulation as well as
         # allow us to select a time. Now the slider just changes the tstep box
@@ -62,7 +63,7 @@ class playbackBar(Tk.Frame):
             self,
             from_=self.param.minimum,
             to=self.param.maximum,
-            command = self.scale_handler)
+            command=self.scale_handler)
 
         self.slider.set(self.param.value)
         self.slider.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
@@ -73,19 +74,17 @@ class playbackBar(Tk.Frame):
         self.loop_var = Tk.IntVar()
         self.loop_var.set(self.oengus.MainParamDict['LoopPlayback'])
         self.loop_var.trace('w', self.loop_changed)
-        self.record_frames = ttk.Checkbutton(new_frame, text = 'Loop',
-                                            variable = self.loop_var)
-        self.record_frames.pack(side=Tk.TOP, fill=Tk.BOTH, expand=0)
-        self.rec_var = Tk.IntVar()
-        self.rec_var.set(self.oengus.MainParamDict['Recording'])
-        self.rec_var.trace('w', self.rec_changed)
-        #ttk.Checkbutton(new_frame, text = 'Record',
-        #                variable = self.rec_var).pack(side=Tk.TOP, fill=Tk.BOTH, expand=0)
-        new_frame.pack(side= Tk.LEFT, fill = Tk.BOTH, expand =0)
+        self.loop_btn = ttk.Checkbutton(
+            new_frame, text='Loop',
+            variable=self.loop_var)
+        self.loop_btn.pack(side=Tk.TOP, fill=Tk.BOTH, expand=0)
+
+        new_frame.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
 
         # a measurement button that should lauch a window to take measurements.
-        #self.measuresB= ttk.Button(self, text='FFT', command=self.open_measures)
-        #self.measuresB.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
+        # self.measuresB = ttk.Button(
+        #    self, text='FFT', command=self.open_measures)
+        # self.measuresB.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
 
         # a settings button that should lauch some global settings.
         self.settingsB = ttk.Button(
@@ -95,15 +94,21 @@ class playbackBar(Tk.Frame):
         self.settingsB.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
 
         # a reload button that reloads the files and then refreshes the plot
-        #ttk.Button(self, text = 'Reload', command = self.on_reload).pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
+        # ttk.Button(
+        #    self, text='Reload',
+        #    command=self.on_reload).pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
         # a refresh button that refreshing the current timestep
-        ttk.Button(self, text = 'Clear Cache', command = self.on_refresh).pack(side=Tk.LEFT, fill=Tk.BOTH, expand=0)
-        #attach the parameter to the Playbackbar
+        ttk.Button(
+            self, text='Clear Cache',
+            command=self.on_refresh).pack(
+                side=Tk.LEFT, fill=Tk.BOTH, expand=0)
+        # attach the parameter to the Playbackbar
         self.param.attach(self)
 
     @property
     def cur_sim(self):
         return self._cur_sim
+
     @cur_sim.setter
     def cur_sim(self, val):
         self._cur_sim = val
@@ -113,8 +118,9 @@ class playbackBar(Tk.Frame):
         menu.delete(0, "end")
         for x in self.oengus.sims_shown:
             name = self.oengus.sims[x].name
-            menu.add_command(label=name,
-                             command=lambda value=name: self.cur_sim_name.set(value))
+            menu.add_command(
+                label=name,
+                command=lambda value=name: self.cur_sim_name.set(value))
 
     def simChanged(self, *args):
         if self.cur_sim_name.get() == self.oengus.sims[self.cur_sim].name:
@@ -123,7 +129,6 @@ class playbackBar(Tk.Frame):
             names = [sim.name for sim in self.oengus.sims]
             self.cur_sim = names.index(self.cur_sim_name.get())
             self.oengus.cur_sim = self.cur_sim
-            # Move the
 
     def open_settings(self):
         if self.settings_window is None:
@@ -132,26 +137,10 @@ class playbackBar(Tk.Frame):
             self.settings_window.destroy()
             self.settings_window = SettingsFrame(self.oengus)
 
-        #print('open_settings')
-    def on_reload(self, *args):
-        print('on_reload')
-    #    self.parent.ReloadPath()
-    #    self.parent.RenewCanvas()
-
     def on_refresh(self, *args):
         for sim in self.oengus.sims:
             sim.clear_caches()
-    #    self.parent.RefreshTimeStep()
-    #    self.parent.RenewCanvas()
-
-    def rec_changed(self, *args):
-        print('rec_changed')
-        #if self.RecVar.get() == self.parent.MainParamDict['Recording']:
-        #    pass
-        #else:
-        #    self.parent.MainParamDict['Recording'] = self.RecVar.get()
-        #    if self.parent.MainParamDict['Recording'] == 1:
-        #        self.parent.PrintFig()
+        self.oengus.draw_output()
 
     def loop_changed(self, *args):
         if self.loop_var.get() == self.oengus.MainParamDict['LoopPlayback']:
@@ -159,64 +148,55 @@ class playbackBar(Tk.Frame):
         else:
             self.oengus.MainParamDict['LoopPlayback'] = self.loop_var.get()
             self.param.loop = self.oengus.MainParamDict['LoopPlayback']
-    def skip_left(self, e = None):
-        self.param.set(self.param.value - self.oengus.MainParamDict['SkipSize'])
 
-    def skip_right(self, e = None):
-        self.param.set(self.param.value + self.oengus.MainParamDict['SkipSize'])
+    def skip_left(self, e=None):
+        self.param.set(
+            self.param.value - self.oengus.MainParamDict['SkipSize'])
 
-    def play_handler(self, e = None):
+    def skip_right(self, e=None):
+        self.param.set(
+            self.param.value + self.oengus.MainParamDict['SkipSize'])
+
+    def play_handler(self, e=None):
         if not self.play_pressed:
             # Set the value of play pressed to true, change the button name to
             # pause, turn off clear_fig, and start the play loop.
             self.play_pressed = True
             self.playB.config(text='Pause')
 
-            self.after(int(self.oengus.MainParamDict['WaitTime']*1E3), self.blink)
+            self.after(
+                int(self.oengus.MainParamDict['WaitTime']*1E3), self.blink)
         else:
-            #self.parent.resizable(1,1)
-            # pause the play loop, turn clear fig back on, and set the button name back to play
+            # pause the play loop, turn clear fig back on,
+            # and set the button name back to play
             self.play_pressed = False
-            #try:
-            #    self.MovieFrame.destroy()
-            #except AttributeError:
-            #    pass
-            #self.parent.RenewCanvas()
-#            self.parent.MainParamDict['ClearFig'] = True
             self.playB.config(text='Play')
-
-
-    def open_measures(self):
-        print('open_measures')
-        """
-        if self.parent.measure_window is None:
-            self.parent.measure_window = MeasureFrame(self.parent)
-        else:
-            self.parent.measure_window.destroy()
-            self.parent.measure_window = MeasureFrame(self.parent)
-        """
 
     def blink(self):
         if self.play_pressed:
             # First check to see if the timestep can get larger
-            if self.param.value == self.param.maximum and not self.oengus.MainParamDict['LoopPlayback']:
+            no_loop = not self.oengus.MainParamDict['LoopPlayback']
+            if self.param.value == self.param.maximum and no_loop:
                 # push pause button
-                self.PlayHandler()
+                self.play_handler()
 
             # otherwise skip right by size skip size
             else:
-                self.param.set(self.param.value + self.oengus.MainParamDict['SkipSize'])
+                self.param.set(
+                    self.param.value + self.oengus.MainParamDict['SkipSize'])
 
             # start loopin'
-            self.after(int(self.oengus.MainParamDict['WaitTime']*1E3), self.blink)
+            self.after(
+                int(self.oengus.MainParamDict['WaitTime']*1E3),
+                self.blink)
 
     def text_callback(self):
         try:
-            #make sure the user types in a int
+            # make sure the user types in a int
             if int(self.tstep.get()) != self.param.value:
                 self.param.set(int(float(self.tstep.get())))
         except ValueError:
-            #if they type in random stuff, just set it ot the param value
+            # if they type in random stuff, just set it ot the param value
             self.tstep.set(str(self.param.value))
 
     def scale_handler(self, e):
@@ -225,13 +205,14 @@ class playbackBar(Tk.Frame):
             if int(self.tstep.get()) != int(self.slider.get()):
                 self.tstep.set(str(int(self.slider.get())))
         except ValueError:
-            #if they type in random stuff, just set it ot the param value
+            # if they type in random stuff, just set it ot the param value
             self.tstep.set(str(int(self.slider.get())))
 
     def update_value(self, *args):
         if int(self.slider.get()) != self.param.value:
             self.param.set(int(self.slider.get()))
+
     def set_knob(self, value):
-        self.slider.config(to =self.param.maximum)
+        self.slider.config(to=self.param.maximum)
         self.slider.set(value)
         self.tstep.set(str(value))
